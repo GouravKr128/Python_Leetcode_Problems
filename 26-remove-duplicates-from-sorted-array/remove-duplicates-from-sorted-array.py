73d003cb55b66defa3923b81bfb1e39f221f8fb2
Gourav Kumar <gk128128@gmail.com>
2026-04-21 21:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (20.26%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,5 +1,24 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums)<2:
+            return len(nums)
+        i=0
+        j=1
+        while j!=len(nums):
+            if nums[i]==nums[j]:
+                j+=1
+            else:
+                i+=1
+                nums[i]=nums[j]
+                j+=1
+        return i+1
+
+
+
+
+""" 
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
         if len(nums)==0:
             return 0
         d={nums[i]:0 for i in range(len(nums))}
@@ -8,7 +27,7 @@ class Solution:
             nums[j]=i
             j+=1
         return j
-
+"""
 
 
 

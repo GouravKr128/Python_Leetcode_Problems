49ea85ea89a44e721b86f1b01293a70ab11d4926
Gourav Kumar <gk128128@gmail.com>
2026-05-02 01:20:46 +0530

    Time: 1506 ms (4.15%) | Memory: 19.4 MB (27.52%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)==0 or len(s)%2!=0:
+            return False
+        l=list(s)
+        d={'(':')','[':']','{':'}',')':0,'}':0,']':0}
+        #l=}}
+        while True:
+            i=0
+            if len(l)==0:
+                return True
+            while True:
+                if d[l[i]]==l[i+1]:
+                    l.pop(i)
+                    l.pop(i)
+                    break
+                else:
+                    i+=1
+                    if i==(len(l)-1):
+                        return False
+            
+        

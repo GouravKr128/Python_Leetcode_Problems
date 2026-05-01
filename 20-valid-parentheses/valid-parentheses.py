cf3e7fe0a6f1b0058b61d5d94ec7015fe119f7cf
Gourav Kumar <gk128128@gmail.com>
2026-05-02 02:22:37 +0530

    Time: 2575 ms (4.15%) | Memory: 18.8 MB (99.95%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,6 +3,28 @@ class Solution:
         if len(s)==0 or len(s)%2!=0:
             return False
         l=list(s)
+        d={'()':0,'[]':0,'{}':0}
+        i=0
+        while True:
+            if len(l)==0:
+                return True
+            if l[i]+l[i+1] in d:
+                l.pop(i)
+                l.pop(i)
+                i=0
+            else:
+                i+=1
+                if i==(len(l)-1):
+                    return False
+
+
+
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)==0 or len(s)%2!=0:
+            return False
+        l=list(s)
         d={'(':')','[':']','{':'}',')':0,'}':0,']':0}
         #l=}}
         while True:
@@ -19,4 +41,4 @@ class Solution:
                     if i==(len(l)-1):
                         return False
             
-        
+"""     
